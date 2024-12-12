437673723b2c78b4867a86a19b095c4341766306
piyushmehra1221 <piyushmehra1221@gmail.com>
2024-12-12 15:39:35 +0530

    if else

---
+++ decision/Ifelse2.java
@@ -0,0 +1,16 @@
+package control_statement;
+
+import java.util.Scanner;
+
+public class Ifelse2 {
+    public static void main(String[] args) {
+        Scanner oc=new Scanner(System.in);
+        int n=oc.nextInt();
+        if(n==9){
+            System.out.println("valid");
+        }
+        else{
+            System.out.println("not valid");
+        }
+    }
+}
