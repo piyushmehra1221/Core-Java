700e9c02a83fcf79cc3115df7ee90a953c927984
piyushmehra1221 <piyushmehra1221@gmail.com>
2024-12-13 15:01:34 +0530

    basic if else

---
+++ BASIC_IF_ELSE/Ifelse14.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Ifelse14 {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter number to check day name");
+        int num=sc.nextInt();
+        if(num==1)
+        {
+            System.out.println("Sunday");
+        }
+        else if(num==2)
+        {
+            System.out.println("Monday");
+        }
+        else if(num==3)
+        {
+            System.out.println("Tuesday");
+        }
+        else if(num==4)
+        {
+            System.out.println("Wednesday");
+        }
+        else if(num==5)
+        {
+            System.out.println("Thrusday");
+        }
+        else if(num==6)
+        {
+            System.out.println("Friday");
+        }
+        else if(num==7)
+        {
+            System.out.println("Saturday");
+        }
+        else
+        {
+            System.out.println("miss match");
+        }
+    }
+    
+}
