0240ea3b704a078ba38f5085a709c9bc0e6645d6
piyushmehra1221 <piyushmehra1221@gmail.com>
2024-12-17 14:51:15 +0530

    Method creat

---
+++ OOPS/Methods.java
@@ -0,0 +1,16 @@
+class A
+{
+    void show()
+    {
+        System.out.println("class A Method ");
+    }
+}
+
+public class Methods
+{
+    public static void main(String[] args) {
+        
+        A k=new A();
+        k.show();
+    }
+}
