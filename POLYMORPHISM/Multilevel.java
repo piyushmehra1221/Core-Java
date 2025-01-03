33bfdb51074987b63283c65c748847ccbb7b34ab
piyushmehra1221 <piyushmehra1221@gmail.com>
2025-01-03 13:44:12 +0530

    multilevel

---
+++ POLYMORPHISM/Multilevel.java
@@ -0,0 +1,43 @@
+class A 
+{
+   void show1()
+   {
+    System.out.println("A method");
+   }
+}
+
+class B extends A
+{
+    void show2()
+   {
+    System.out.println("B method");
+   }
+}
+
+class C extends B
+{
+    void show3()
+   {
+    System.out.println("C method");
+   }
+}
+
+class D extends C
+{
+    void show4()
+   {
+    System.out.println("D method");
+   }
+}
+
+
+public class Multilevel {
+    
+    public static void main(String[] args) {
+
+      
+      
+
+        
+    }
+}
