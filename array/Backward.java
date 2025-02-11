b7aa4855736234a7d3a41ae5a2962c2b9cae5465
piyushmehra1221 <piyushmehra1221@gmail.com>
2025-02-11 14:36:48 +0530

    backward print a number

---
+++ array/Backward.java
@@ -0,0 +1,9 @@
+public class Backward {
+    public static void main(String[] args) {
+        int []a={12,67,46,435,67,385,335,335,34};
+        for(int i=8;i>=0;i--)
+        {
+            System.out.print(a[i]+" ");
+        }
+    }
+}
