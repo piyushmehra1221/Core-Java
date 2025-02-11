3cda8a6ebdc96b7b660c3b9d3c4ba876f033492b
piyushmehra1221 <piyushmehra1221@gmail.com>
2025-02-11 14:28:10 +0530

    char value length find in array

---
+++ array/Char.java
@@ -0,0 +1,9 @@
+public class Char {
+    public static void main(String[] args) {
+        char[] a={'a','e','i','o','u'};
+        System.out.println(a[3]);
+
+        float[] r={4.4f,3.3f,2.2f,1.1f};
+        System.out.println(a.length); 
+    }
+}
