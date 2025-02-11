3fa8e4edba80230110002ced4e1e654940e40d8b
piyushmehra1221 <piyushmehra1221@gmail.com>
2025-02-11 14:56:37 +0530

    mid element in arrya

---
+++ array/Midnumber.java
@@ -0,0 +1,13 @@
+public class Midnumber {
+    public static void main(String[] args) {
+        int []a={12,67,46,435,67,385,335,335,34};
+      
+        for(int i=0;i<a.length;i++)
+        {
+            
+           System.out.print(a[i]/2+" ");
+        
+           
+        }
+    }
+}
