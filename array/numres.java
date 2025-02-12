aed74a3a52f91c2f830282a36e580459ea647e24
piyushmehra1221 <piyushmehra1221@gmail.com>
2025-02-12 14:22:36 +0530

    half number revers in

---
+++ array/numres.java
@@ -0,0 +1,17 @@
+public class numres {
+    public static void main(String[] args) {
+        int num[]={12,67,46,435,67,385,33,335,34,23};
+
+        for(int i=0;i<=4;i++)
+        {
+            System.out.print(num[i]+" ");
+            
+            
+        }
+        for(int j=9;j>4;j--)
+            {
+                System.out.print(num[j]+" ");
+            }
+        
+    }
+}
